8242e2f7636c2f1d6b89cf5b871464880db69b3f
RaynierPM <raynierminyety@gmail.com>
2025-04-10 20:06:01 -0400

    Logic fixed

--- src/common/utils/randomAnswers.ts
+++ src/common/utils/randomAnswers.ts
@@ -3,7 +3,9 @@ import { t } from "../../i18n";
 
 export abstract class RandomAnswers {
   static GET_RANDOM_PING_INDEX() {
-    const randomIndex = Math.floor(Math.random() * config.bot.available_pings);
+    const randomIndex = Math.floor(
+      Math.random() * config.bot.available_pings - 1,
+    );
     return "bot.PING." + randomIndex;
   }
 }
